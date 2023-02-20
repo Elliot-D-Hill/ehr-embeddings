7ab38d3b3bb515a74af88f822864b443e89767a4
Elliot Hill <elliot.douglas.hill@gmail.com>
2023-02-20 12:45:38 -0500

    Add pytorch lightning and pytorch metric learning

--- src/ehrembeddings/__main__.py
+++ src/ehrembeddings/__main__.py
@@ -1,55 +1,76 @@
-from functools import partial
 import toml
 from box import Box
 from polars import read_csv
-from torch import arange, cat, backends, device as torch_device
-from torch.utils.data import DataLoader, TensorDataset
-from torch.optim import AdamW
-from pytorch_metric_learning.losses import NTXentLoss
-from ehrembeddings.model import CBOW
-from ehrembeddings.preprocess import collate_fn, make_data, make_vocabulary
+from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    LearningRateMonitor,
+    RichProgressBar,
+)
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch import tensor
+
+from ehrembeddings.model import ICD2Vec
+from ehrembeddings.dataset import EmbeddingsDataModule, make_data, make_vocabulary
 
 
 def main():
     config = Box(toml.load("config.toml"))
-    df = read_csv(config.data_path)
+    seed_everything(config.random_seed)
+    df = read_csv(config.paths.data)
     vocabulary = make_vocabulary(corpus=df["icd_code"])
     df = df.join(vocabulary, on="icd_code")
-    contexts, targets = make_data(df=df)
-    dataset = TensorDataset(contexts, targets)
-    device_type = "mps" if backends.mps.is_available() else "cpu"
-    device = torch_device(device_type)
-    collate_batch = partial(collate_fn, device=device)
-    dataloader = DataLoader(
-        dataset,
-        batch_size=config.batch_size,
-        shuffle=True,
-        collate_fn=collate_batch,
-        pin_memory=True,
-    )
+    data = make_data(df=df)
+    contexts = tensor(data["context"].to_numpy()).long()
+    targets = tensor(data["target"].to_numpy()).long()
     vocab_size = vocabulary.shape[0]
-    model = CBOW(vocab_size=vocab_size, embedding_dim=config.embedding_dim)
-    model.to(device)
-    criterion = NTXentLoss(temperature=config.temperature)
-    optimizer = AdamW(model.parameters(), lr=config.learning_rate)
-    num_epochs = 10
-    for epoch in range(num_epochs):
-        for i, (inputs, outputs) in enumerate(dataloader):
-            optimizer.zero_grad()
-            input_embeddings, output_embeddings = model(inputs, outputs)
-            embeddings = cat((input_embeddings, output_embeddings))
-            # for NTXent loss, labels that are equal are positive pairs
-            # and labels that are not equal on negative pairs
-            labels = arange(input_embeddings.shape[0]).repeat(2)
-            loss = criterion(embeddings, labels)
-            loss.backward()
-            optimizer.step()
-            if i % 10 == 0:
-                print(loss.item())
+    model = ICD2Vec(
+        vocab_size=vocab_size,
+        **config.model,
+        **config.loss,
+        **config.optimizer,
+        **config.lr_scheduler,
+    )
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=config.paths.checkpoints,
+        filename="{epoch}_{step}_{train_loss:.2f}",
+        save_top_k=1,
+        verbose=True,
+        monitor="train_loss",
+        mode="min",
+        every_n_train_steps=config.logging.checkpoint_every_n_steps,
+    )
+    logger = TensorBoardLogger(
+        config.paths.logs,
+        name="embeddings",
+    )
+    callbacks = [
+        checkpoint_callback,
+        LearningRateMonitor(logging_interval="step"),
+        RichProgressBar(),
+    ]
+    trainer = Trainer(
+        logger=logger,
+        callbacks=callbacks,
+        gradient_clip_val=config.training.gradient_clip,
+        max_epochs=config.training.max_epochs,
+        auto_lr_find=True,
+        accelerator="cpu",  # FIXME should be "auto" eventually
+        devices="auto",
+        log_every_n_steps=config.logging.log_every_n_steps,
+        track_grad_norm=2,
+        fast_dev_run=config.fast_dev_run,
+    )
+    data_module = EmbeddingsDataModule(
+        contexts=contexts, targets=targets, batch_size=config.training.batch_size
+    )
+    trainer.tune(model, datamodule=data_module)
+    trainer.fit(
+        model,
+        datamodule=data_module,
+        # ckpt_path="",
+    )
 
-
-# Save the trained model
-# torch.save(model.state_dict(), 'word2vec_model.pt')
 
 if __name__ == "__main__":
     main()
